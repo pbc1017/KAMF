fb8c9265536adf7845351671ac9b39701a3aca5d
perry <perry@channel.io>
2025-08-30 20:56:19 +0900

    Remove Auth and Users modules along with their controllers and services from the API, simplifying the application structure.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,10 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import databaseConfig from './config/database.config.js';
-import { AuthModule } from './modules/auth/auth.module.js';
 import { BoothModule } from './modules/booth/booth.module.js';
 import { StageModule } from './modules/stage/stage.module.js';
-import { UsersModule } from './modules/users/users.module.js';
 
 @Module({
   imports: [
@@ -21,8 +19,6 @@ import { UsersModule } from './modules/users/users.module.js';
       useFactory: (configService: ConfigService) => configService.get('database'),
       inject: [ConfigService],
     }),
-    UsersModule,
-    AuthModule,
     BoothModule,
     StageModule,
   ],
